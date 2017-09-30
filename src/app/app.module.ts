7fc2998144526148387d71932a0cb8a9f17c10d5
JesseSoldat <jsoldat@hotmail.com>
2017-09-30 11:17:22 -0400

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { routerConfig } from './router.config';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+//3rd party
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { firebaseConfig } from '../environments/firebase.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    TopMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routerConfig),
+    //3rd party
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { User } from '../shared/model/user';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Http, Headers } from '@angular/http';
